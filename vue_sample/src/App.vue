b208ac3e0fc92970bae337df7b2de8d99f63fe08
Ito Fumitaka <ffffffiffffffffffff@yahoo.co.jp>
2021-02-12 15:49:21 +0900

    add vuecli

---
+++ vue_sample/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- 大文字で始まるパスカルケースで記載。like-headerというケバブケースでも記載可 -->
+    <LikeHeader></LikeHeader>
+    <LikeNumber></LikeNumber>
+  </div>
+</template>
+<script>
+// ローカル登録
+import LikeHeader from "./components/LikeHeader.vue"
+export default {
+  components: {
+    LikeHeader: LikeHeader
+  }
+}
+</script>
+// scopedを入れる事により他のvueファイルへのスタイル適用を防ぐ。
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
+</style> 
---
+++ vue_sample/src/components/LikeNumber.vue
@@ -0,0 +1,20 @@
+// templateタグの中身は同階層のタグがあってはいけない。
+<template>
+  <div>いいね({{number}})</div>
+</template>
+
+<script>
+
+export default {
+  data: function() {
+    return {
+      number: 57
+    }
+  }
+}
+</script>
+<style scoped>
+  div {
+    border: 1px solid blue;
+  }
+</style> 
