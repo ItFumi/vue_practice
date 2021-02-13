39d784695e69aee9e5aa20c51a20f837da61e0c9
Ito Fumitaka <ffffffiffffffffffff@yahoo.co.jp>
2021-02-13 23:45:26 +0900

    learning section7

--- vue_sample/src/App.vue
+++ vue_sample/src/App.vue
@@ -2,15 +2,34 @@
   <div>
     <!-- 大文字で始まるパスカルケースで記載。like-headerというケバブケースでも記載可 -->
     <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber>
+    <p>{{number}}</p>
+    
+    <!-- HTML内では属性値をケバブケースで記載する事 -->
+    <!-- LikeNumber.vueのincrement()で発火 -->
+    <LikeNumber :total-number="number" @my-click="number = $event"></LikeNumber>
+
+    <!-- @my-click内は関数でも可能 -->
+    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
+
+    <LikeNumber :total-number="number" test-props="test"></LikeNumber>
   </div>
 </template>
 <script>
 // ローカル登録
 import LikeHeader from "./components/LikeHeader.vue"
 export default {
+  data(){
+    return {
+      number: 10
+    }
+  },
   components: {
     LikeHeader: LikeHeader
+  },
+  methods: {
+    incrementNumber(val) {
+      this.number = val;
+    }
   }
 }
 </script>
--- vue_sample/src/components/LikeNumber.vue
+++ vue_sample/src/components/LikeNumber.vue
@@ -1,20 +1,46 @@
-// templateタグの中身は同階層のタグがあってはいけない。
+//templateタグの中身は同階層のタグがあってはいけない。
 <template>
-  <div>いいね({{number}})</div>
+	<div>
+		<div>いいね({{totalNumber/2}})</div>
+		<div>いいね({{halfNumber}})</div>
+		<button @click="increment">+1</button>
+	</div>
 </template>
-
 <script>
-
 export default {
-  data: function() {
-    return {
-      number: 57
-    }
-  }
+	//下記は「props: ["totalNumber","testProps"],」といった配列でも記載可能
+	props: {
+		totalNumber: {
+			totalNumber: Number, //「type: Number」でも可能
+			//「required: true」で入力必須かどうか
+			//「default: 10」で何も入力されなかった場合の値。requiredとは一緒に記載できない。
+			/* オブジェクト型の場合
+			default: function() {
+				return {
+					number: 10
+				}
+			}
+			*/
+		},
+		testProps: {
+			type: String
+		}
+	},
+	computed: {
+		halfNumber() {
+			return this.totalNumber / 2;
+		}
+	},
+	methods: {
+		increment() {
+			// $emitは子から任意のタイミングでイベントを発火するもの。第一引数のみでも可能。
+			this.$emit("my-click",this.totalNumber + 1);
+		}
+	}
 }
 </script>
 <style scoped>
-  div {
-    border: 1px solid blue;
-  }
+div {
+	border: 1px solid blue;
+}
 </style> 
