5e0b413ce75b6e4d84e597a0c03b6266a1d9049f
Ito Fumitaka <ffffffiffffffffffff@yahoo.co.jp>
2021-02-19 09:03:07 +0900

    learning section9

--- vue_sample/src/App.vue
+++ vue_sample/src/App.vue
@@ -42,8 +42,8 @@
         <p>え</p>
         <!-- 下記dataのtitleをv-slot[title]に挿入し、Section8.vueへhtmlタグも含めたテキストを送る。 -->
         <template v-slot:[title]>
-            <p>犬</p>
-            <p>猫</p>
+          <p>犬</p>
+          <p>猫</p>
         </template>
       </Section8>
       <!-- タグ内に何も挿入していない為、Section8_1.vueのslotタグ内のテキストを表示する。 -->
@@ -64,29 +64,105 @@
         <component :is="currentComponent"></component>
       </keep-alive>
     </div>
+    <div class="section">
+      <h3>イベントのフォーム</h3>
+      <p>
+        <!-- .lazyなしの場合、リアルタイムで入力値が変更される。 -->
+        <label for="title">タイトル(.lazyなし)</label>
+        <input id="title" type="text" v-model="eventData.title">
+        {{eventData.title}}
+      </p>
+      <p>
+        <!-- .lazyありの場合、Enterキーで確定時に入力値が変更される。 -->
+        <label for="title2">タイトル2(.lazyあり)</label>
+        <input id="title2" type="text" v-model.lazy="eventData.title2">
+        {{eventData.title2}}
+      </p>
+      <p>
+        <!-- コンポーネントに記載する方法。初期値の0を除き、値を変更するとString型に変更されるのを防ぐ為に.numberを使用する。 -->
+        <!-- 下記v-modelは:value="eventData.maxNumber"に変換できる。 -->
+        <Section9 v-model.number="eventData.maxNumber"></Section9>
+      </p>
+      <p>
+        <!-- .trimで入力値の前後の空白を削除する。 -->
+        <label for="host">主催者</label>
+        <input id="host" type="text" v-model.trim="eventData.host">
+        {{eventData.host}}
+      </p>
+      <p>
+        <label for="detail">イベントの内容</label>
+        <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
+        {{eventData.detail}}
+      </p>
+      <p>
+        <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+        <label for="isPrivate">非公開</label>
+        {{eventData.isPrivate}}
+      </p>
+      <p>
+        <!-- 下記のv-modelにdataの共通のプロパティ名を指定すると関連づけられる。 -->
+        <input type="checkbox" id="10" v-model="eventData.target">
+        <label for="10">10代</label>
+        <input type="checkbox" id="20" v-model="eventData.target">
+        <label for="20">20代</label>
+        <input type="checkbox" id="30" v-model="eventData.target">
+        <label for="30">30代</label>
+      </p>
+      <p>
+        <span>参加費</span>
+        <!-- 下記のv-modelにdataの共通のプロパティ名を指定すると関連づけられる。 -->
+        <input type="radio" id="free" value="無料" v-model="eventData.price"><label for="free">無料</label>
+        <input type="radio" id="paid" value="有料" v-model="eventData.price"><label for="paid">有料</label>
+      </p>
+      <p>
+        <!-- 複数選択の場合、下記selectタグにmultipleを挿入し、dataのlocationを[]に変更する。 -->
+        <select v-model="eventData.location">
+          <option v-for="location in locations" :key="location">{{location}}</option>
+        </select>
+        {{eventData.location}}
+      </p>
+    </div>
   </div>
 </template>
 <script>
 // コンポーネントをローカル登録する。App.vueのみ使用可能。
 import Section6_1 from './components/Section6_1.vue'
+import Section7 from './components/Section7.vue'
+import Section8 from './components/Section8.vue'
 import Section8_1 from './components/Section8_1.vue'
 import Section8_2 from './components/Section8_2.vue'
 import Section8_3 from './components/Section8_3.vue'
 import Section8_4 from './components/Section8_4.vue'
+import Section9 from './components/Section9.vue'
 export default {
   data() {
     return {
       number: 10,
       title: "animal",
-      currentComponent: 'Section8_3'
+      currentComponent: 'Section8_3',
+      locations: ["東京","大阪"],
+      eventData: {
+        title: "タイトル",
+        title2: "タイトル2",
+        maxNumber: 0,
+        host: "",
+        detail: "",
+        isPrivate: false,
+        target: [],
+        price: "無料",
+        location: "東京"
+      }
     }
   },
   components: {
     Section6_1,
+    Section7,
+    Section8,
     Section8_1,
     Section8_2,
     Section8_3,
-    Section8_4
+    Section8_4,
+    Section9
   },
   methods: {
     // 子コンポーネントのSection7.vueのincrement関数内にある$emitの第二引数のデータがvalueに挿入される。
---
+++ vue_sample/src/components/Section9.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<p>
+			<!-- 初期値の0を除き、値を変更するとString型に変更されるのを防ぐ為に.numberを使用する。 -->
+			<label for="maxNumber">最大人数</label>
+			<input id="maxNumber" type="number" :value="value" @input="$emit('input',$event.target.value)">
+			{{value}} {{typeof value}}
+		</p>
+	</div>
+</template>
+<script>
+export default {
+	// App.vueの同一の属性名のテキストを受取る。
+	props: ["value"]
+}
+</script>
